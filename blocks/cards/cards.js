c0bdaba2fa949ee016c0f0de8cdc4a1a7b45ff5d
= <42250657+JoePearlAdobe@users.noreply.github.com>
2023-05-31 10:38:57 -0400

    testing

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -4,6 +4,7 @@ import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
   console.log("hello world");
 
 </script>
+
 export default function decorate(block) {
   /* change to ul, li */
   const ul = document.createElement('ul');
